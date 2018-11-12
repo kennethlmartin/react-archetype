10bd7f9d7876754a5c8a2d64c6931b48c4f2d6a2
Kenneth Martin <kennethlmartin@gmail.com>
2018-11-11 19:06:06 -0500

    cleanup convict configuration index

--- config/index.js
+++ config/index.js
@@ -4,23 +4,16 @@
 
 const convict = require('convict');
 
-const app = require('config/app');
-const cookieKeys = require('config/cookie-keys');
-const cookieSession = require('config/cookie-session');
-const dirs = require('config/dirs');
-const files = require('config/files');
-const webpack = require('config/webpack');
-
 module.exports = convict({
-  app,
-  cookieKeys,
-  cookieSession,
-  dirs,
+  app: require('./app'),
+  cookieKeys: require('./cookie-keys'),
+  cookieSession: require('./cookie-session'),
+  dirs: require('./dirs'),
   env: {
     default: 'development',
     env: 'NODE_ENV',
     format: ['production', 'staging', 'development', 'test'],
   },
-  files,
-  webpack,
+  files: require('./files'),
+  webpack: require('./webpack'),
 });
