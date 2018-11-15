361dc47e9865df68f680c5db963a654b3753173e
Kenneth Martin <kennethlmartin@gmail.com>
2018-11-14 23:54:00 -0500

    remove unused config `asyncAssetManifest`

--- config/files.js
+++ config/files.js
@@ -11,10 +11,6 @@ module.exports = {
     doc: 'name of asset-manifest file produced by webpack',
     format: String,
   },
-  asyncAssetManifest: {
-    default: path.join(dirs.build.default, 'async-asset-manifest.json'),
-    format: String,
-  },
   entryScript: {
     default: path.join(dirs.app.default, 'Client.jsx'),
     doc: 'client-side entry file for the app',
