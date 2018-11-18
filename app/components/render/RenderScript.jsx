da71de4a4118cbf312a8e4ec778e656584228344
Kenneth Martin <kennethlmartin@gmail.com>
2018-11-17 23:14:14 -0500

    add async prop to RenderScript component

--- app/components/render/RenderScript.jsx
+++ app/components/render/RenderScript.jsx
@@ -6,16 +6,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 /* eslint-disable react/no-danger */
-const RenderScript = ({ children, ...props }) => {
+const RenderScript = ({ async, children, ...props }) => {
   if (children) {
     return <script dangerouslySetInnerHTML={{ __html: children }} />;
   }
 
-  return <script {...props} />;
+  return <script async={async} {...props} />;
 };
 
 RenderScript.propTypes = {
   children: PropTypes.string,
+};
+
+RenderScript.defaultProps = {
+  async: true,
 };
 
 export default RenderScript;
