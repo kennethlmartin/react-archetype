9a0c5548f54544b054e6c06c8f412c081e246181
Kenneth Martin <kennethlmartin@gmail.com>
2018-11-17 23:16:06 -0500

    remove deprecated basename state prop

--- server/middleware/preload-state.js
+++ server/middleware/preload-state.js
@@ -7,7 +7,6 @@ const config = require('config');
 module.exports = async (ctx, next) => {
   ctx.preloadedState = {
     app: {
-      basename: config.get('app.basePath'),
       basePath: config.get('app.basePath'),
     },
   };
