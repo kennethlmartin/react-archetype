075fe133ad6cb0c3907015c38e78db7c7a705181
Kenneth Martin <developer@kennethmartin.me>
2019-04-03 23:33:21 -0400

    remove unused script components

--- app/components/render/index.js
+++ app/components/render/index.js
@@ -4,5 +4,4 @@
 
 export { default as RenderIf } from './RenderIf';
 export { default as RenderMarkup } from './RenderMarkup';
-export { default as RenderScript } from './RenderScript';
 export { default as RenderStylesheet } from './RenderStylesheet';
--- app/components/scripts/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @module app/components/scripts
- */
-
-export { default as JQueryScript } from './JQueryScript';
-export { default as PolyfillScript } from './PolyfillScript';
-export { default as PreloadedState } from './PreloadedState';
