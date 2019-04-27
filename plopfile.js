f2bd1e467e7c8c3fb10ac5f92b1bbbe633b5dcae
Kenneth Martin <developer@kennethmartin.me>
2019-04-26 21:00:55 -0400

    improve plop templating

--- plopfile.js
+++ plopfile.js
@@ -3,27 +3,27 @@
  * @see https://plopjs.com/documentation
  */
 
-module.exports = function (plop) {
+module.exports = function(plop) {
   plop.setGenerator('component', {
     actions: [{
-      path: 'app/components/{{ pascalCase name }}/index.js',
-      templateFile: 'templates/app/components/Example/index.js.hbs',
+      path: 'app/components/{{ lowerCase path }}{{ pascalCase name }}/index.js',
+      templateFile: 'templates/components/Example/index.js.hbs',
       type: 'add',
     }, {
-      path: 'app/components/{{ pascalCase name }}/{{ pascalCase name }}.css',
-      templateFile: 'templates/app/components/Example/Example.css.hbs',
+      path: 'app/components/{{ lowerCase path }}{{ pascalCase name }}/{{ pascalCase name }}.css',
+      templateFile: 'templates/components/Example/Example.css.hbs',
       type: 'add',
     }, {
-      path: 'app/components/{{ pascalCase name }}/{{ pascalCase name }}.jsx',
-      templateFile: 'templates/app/components/Example/Example.jsx.hbs',
+      path: 'app/components/{{ lowerCase path }}{{ pascalCase name }}/{{ pascalCase name }}.jsx',
+      templateFile: 'templates/components/Example/Example.jsx.hbs',
       type: 'add',
     }, {
-      path: 'app/components/{{ pascalCase name }}/{{ pascalCase name }}.spec.js',
-      templateFile: 'templates/app/components/Example/Example.spec.js.hbs',
+      path: 'app/components/{{ lowerCase path }}{{ pascalCase name }}/{{ pascalCase name }}.spec.js',
+      templateFile: 'templates/components/Example/Example.spec.js.hbs',
       type: 'add',
     }, {
-      path: 'app/components/{{ pascalCase name }}/{{ pascalCase name }}.stories.js',
-      templateFile: 'templates/app/components/Example/Example.stories.js.hbs',
+      path: 'app/components/{{ lowerCase path }}{{ pascalCase name }}/{{ pascalCase name }}.stories.js',
+      templateFile: 'templates/components/Example/Example.stories.js.hbs',
       type: 'add',
     }],
     description: 'Generates a new component.',
@@ -31,12 +31,17 @@ module.exports = function (plop) {
       message: 'component name:',
       name: 'name',
       type: 'input',
+    }, {
+      filter: value => value ? `${value}/` : '',
+      message: 'component path:',
+      name: 'path',
+      type: 'input',
     }],
   });
   plop.setGenerator('container', {
     actions: [{
       path: 'app/containers/{{ pascalCase name }}.jsx',
-      templateFile: 'templates/app/containers/Example.jsx.hbs',
+      templateFile: 'templates/containers/Example.jsx.hbs',
       type: 'add',
     }],
     description: 'Generates a new container component.',
@@ -49,15 +54,15 @@ module.exports = function (plop) {
   plop.setGenerator('page', {
     actions: [{
       path: 'app/pages/{{ pascalCase name }}/index.js',
-      templateFile: 'templates/app/pages/Example/index.js.hbs',
+      templateFile: 'templates/pages/Example/index.js.hbs',
       type: 'add',
     }, {
       path: 'app/pages/{{ pascalCase name }}/{{ pascalCase name }}.css',
-      templateFile: 'templates/app/pages/Example/Example.css.hbs',
+      templateFile: 'templates/pages/Example/Example.css.hbs',
       type: 'add',
     }, {
       path: 'app/pages/{{ pascalCase name }}/{{ pascalCase name }}.jsx',
-      templateFile: 'templates/app/pages/Example/Example.jsx.hbs',
+      templateFile: 'templates/pages/Example/Example.jsx.hbs',
       type: 'add',
     }],
     description: 'Generates a new page component.',
@@ -70,31 +75,31 @@ module.exports = function (plop) {
   plop.setGenerator('state', {
     actions: [{
       path: 'app/state/{{ dashCase name }}/index.js',
-      templateFile: 'templates/app/state/example/index.js.hbs',
+      templateFile: 'templates/state/example/index.js.hbs',
       type: 'add',
     }, {
       path: 'app/state/{{ dashCase name }}/actions.js',
-      templateFile: 'templates/app/state/example/actions.js.hbs',
+      templateFile: 'templates/state/example/actions.js.hbs',
       type: 'add',
     }, {
       path: 'app/state/{{ dashCase name }}/reducer.js',
-      templateFile: 'templates/app/state/example/reducer.js.hbs',
+      templateFile: 'templates/state/example/reducer.js.hbs',
       type: 'add',
     }, {
       path: 'app/state/{{ dashCase name }}/sagas.js',
-      templateFile: 'templates/app/state/example/sagas.js.hbs',
+      templateFile: 'templates/state/example/sagas.js.hbs',
       type: 'add',
     }, {
       path: 'app/state/{{ dashCase name }}/selectors.js',
-      templateFile: 'templates/app/state/example/selectors.js.hbs',
+      templateFile: 'templates/state/example/selectors.js.hbs',
       type: 'add',
     }, {
       path: 'app/state/{{ dashCase name }}/types.js',
-      templateFile: 'templates/app/state/example/types.js.hbs',
+      templateFile: 'templates/state/example/types.js.hbs',
       type: 'add',
     }, {
       path: 'app/state/{{ dashCase name }}/utilities.js',
-      templateFile: 'templates/app/state/example/utilities.js.hbs',
+      templateFile: 'templates/state/example/utilities.js.hbs',
       type: 'add',
     }],
     description: 'Generates a new state directory.',
